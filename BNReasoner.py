77899ebf90066f9f78d56ab3c0d055424574110a
EllisDonderss <ellisdonders@hotmail.com>
2022-12-10 14:24:06 +0100

    min-fill, min-degree

--- BNReasoner.py
+++ BNReasoner.py
@@ -1,7 +1,7 @@
 from typing import Union
 from BayesNet import BayesNet
 import networkx as nx
-
+from itertools import combinations
 
 class BNReasoner:
     def __init__(self, net: Union[str, BayesNet]):
@@ -18,23 +18,27 @@ class BNReasoner:
     # TODO: This is where your methods should go
 
     def sequence(self, path):
+        #code for sequence
         _x, _y, _z = path
         A = _y in self.bn.get_children(_x)
         B = _z in self.bn.get_children(_z)
         return A and B 
 
     def fork(self, path):
+        #code for fork
         _x, _y, _z = path
         AB =  self.bn.get_children(_y) == (_x, _z)
         return AB
 
     def collider(self, path):
+        #code for collider
         _x, _y, _z = path
         A = _y in self.bn.get_children(_x)
         B = _y in self.bn.get_children(_z)
         return A and B
 
     def path_is_closed(self, path, z):
+        #check if the before functions hold, if so, path is closed
         if self.sequence(path):
             if z == path[1]:
                 return True
@@ -52,6 +56,7 @@ class BNReasoner:
                 return False
 
     def d_seperated(self, x, y, evidence):
+        #apply path_is_closed function on the input variables and see whether x is seperated from y given evidence. 
         graph = self.bn.get_interaction_graph()
         all_paths = list(nx.algorithms.all_simple_paths(graph, x, y))
         for path in all_paths:
@@ -62,9 +67,67 @@ class BNReasoner:
         return True
 
     def independent(self, x, y, z):
-        return self.d_seperated(x, y, z)
+        if self.d_seperated(x, y, z):
+            print("{x} is independent from {y} given {z}")
+            return True
+        else:
+            print("{x} is dependent from {y} given {z}")
+            return False
+
+    def min_degree(self, x: list):
+        graph = self.bn.get_interaction_graph()
+        nodes = {}
+        elimination_order = []
+
+        #how many neighbours do the variables have
+        for i in x:
+            nodes[i] = len(list(graph.neighbors(i)))
+        
+        #which node has the least amount of edges and put it in elimination order
+        for j in range(len(x)):
+            next_node = min(nodes.keys(), key = lambda i: nodes[i])
+            nodes.pop(next_node)
+            elimination_order.append(next_node)
+
+        #before removing it, get a list of all the neighbours from the node
+        neighbours = list(graph.neighbors(next_node))
+
+        #remove node with least amount of edges from graph
+        graph.remove_node(next_node)
+
+        #look for all neighbours and add an edge if necessary
+        for pos in list(combinations(neighbours, 2)):
+            if not graph.has_edge(pos[0], pos[1]):
+                graph.add_edge(pos[0], pos[1])
+        
+        return elimination_order
 
 
+    def min_fill(self, x: list):
+        graph = self.bn.get_interaction_graph()
+        elimination_order = []
+        
+        #create dictionary
+        dict_edges = {}
+        for node in x:
+            dict_edges[node] = 0
+
+        #iterate through the variables 
+        for i in range(len(x)):
+            for node in dict_edges.keys():
+                neighbours = list(graph.neighbors(node))
+                #add number of edges to dictionary
+                for pos in list(combinations(neighbours, 2)):
+                    if not graph.has_edge(pos[0], pos[1]):
+                        dict_edges[node] += 1
+            #which node has the fewest edges
+            next_node = min(dict_edges.keys(), key = lambda i: dict_edges[i])
+            #put it in the list of elimination order
+            elimination_order.append(next_node)
+            #delete this node and start again with the next node
+            dict_edges.pop(next_node)
+
+        return elimination_order
 
 #Pruning
 def prune(net, q, e):
@@ -100,15 +163,4 @@ def node_prune(net, q, e): #Performs Node Pruning given query q and evidence e
     #       totalp = sum(cpt['p'])
 
 
-
-def min_degree_heuristic(graph):
-    nx.approximation.treewidth_min_degree(graph)
-    return graph
-
-def min_fill_heur(graph):
-    print(nx.approximation.treewidth_min_fill_in(graph))
-    return graph
-
-
-net = BNReasoner("C:/Users/Ellis/Documents/VU/Knowledge Representation/KR21_project2-main/testing/dog_problem.BIFXML")
 #marginalization(net.bn, [], [])
